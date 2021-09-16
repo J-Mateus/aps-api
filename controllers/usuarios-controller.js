2bf59da96b233df9d0a92d1888008562b58cda3e
José Matioli <j-mateusalves@hotmail.com>
2021-09-16 17:56:49 -0300

    Cadastro de usuários

--- controllers/usuarios-controller.js
+++ controllers/usuarios-controller.js
@@ -1,3 +1,56 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const mysql = require('../mysql').pool
+
+exports.cadastroUsuarios = (req, res, next) => {
+
+    mysql.getConnection((err, conn) => {
+        if(err) {return res.status(500).send({ error: error })}
+
+        conn.query('SELECT * FROM usuarios WHERE email = ?', [req.body.email], (error, results) => {
+            if(error) {return res.status(500).send({ error: error })}
+            if(results.length > 0) {
+                conn.release()
+                res.status(409).send({ mensagem: 'Usuário já cadastrado'})
+            }else {
+                bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+                    if(errBcrypt) { return res.status(500).send({ error: errBcrypt })}
+                    
+                    console.log(req.body.nome,
+                        req.body.email,
+                        hash);
+
+                    conn.query(`INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)`, 
+                    [
+                        req.body.nome,
+                        req.body.email,
+                        hash
+                    ],
+                    (error, result) => {
+                        conn.release()
+                        if(errBcrypt) { return res.status(500).send({ error: errBcrypt })}
+                        
+                        
+
+                        const response = {
+                            mensagem: 'Usuário criado com sucesso',
+                            usuarioCriado: {
+                                id_usuario: result.insertId,
+                                nome: req.body.nome,
+                                email: req.body.email
+                            }
+                        }
+                        return res.status(201).send(response)
+                    })
+                })
+            }
+
+        })
+
+    })
+}
+
+
 exports.loginUsuarios = (req, res, next) => {
 
     const dados = req.body
@@ -8,12 +61,3 @@ exports.loginUsuarios = (req, res, next) => {
     })
 }
 
-exports.cadastroUsuarios = (req, res, next) => {
-
-    const dados = req.body
-
-    return res.status(200).send({
-        mensagem: 'CADASTRO tudo nos conformes',
-        usuario: dados
-    })
-}
