5efc1ff4e0fe802457148ad4567c5e1a49e8119d
José Matioli <j-mateusalves@hotmail.com>
2021-09-17 19:57:11 -0300

    instalando cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,25 +9,10 @@ const rotaUsuarios = require('./routes/usuarios')
 
 app.use(morgan('dev'))
 
-// app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res
-            .status(200)
-            .send({})
-    }
-
-    next()
-})
+app.use(cors())
 
 app.use('/usuarios', rotaUsuarios)
 app.use('/denuncias', rotaDenuncias)
